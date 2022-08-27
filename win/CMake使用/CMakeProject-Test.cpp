8ca2b62202c98c19c0178ffd52a4958f50557419
JeffCheng95 <2811641321@qq.com>
2022-08-27 17:06:55 +0800

    增加CMake demo使用

---
+++ win/CMake使用/CMakeProject-Test.cpp
@@ -0,0 +1,17 @@
+﻿// CMakeProject-Test.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject-Test.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int a = 5;
+	string str("hello");
+	auto b = str.c_str();
+	cout << a << b << endl;
+	return 0;
+}
